24d158e4b6ab751e469d23bd10bf276a1c4771f2
carloshenriqueok <ifcarloshenrique@gmail.com>
2026-03-24 15:55:05 -0300

    FEAT: Criação da model de Veículo

--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -3,7 +3,7 @@ from core.models import Modelo, Cor, Acessorio
 
 class Veiculo(models.Model):
     ano = models.IntegerField(null=True, default=0)
-    preco = models.DecimalField(max_digits=7, decimal_places=2, null=True, default=0)
+    preco = models.DecimalField(max_digits=9, decimal_places=2, null=True, default=0)
     modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculos")
     cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
     acessorios = models.ManyToManyField(Acessorio, related_name="veiculos")
