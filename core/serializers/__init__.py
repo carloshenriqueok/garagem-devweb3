853f17e807c3ce5381bce7e9be56bbec2ca714cf
carloshenriqueok <ifcarloshenrique@gmail.com>
2026-03-24 16:17:51 -0300

    FEAT: Criação da API de veiculo

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -1,4 +1,5 @@
-from .user import UserRegistrationSerializer, UserSerializer
 from .acessorio import AcessorioSerializer
 from .cor import CorSerializer
 from .modelo import ModeloSerializer
+from .user import UserRegistrationSerializer, UserSerializer
+from .veiculo import VeiculoSerializer
